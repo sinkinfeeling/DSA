4091582416f895b5cfdc3cb8a0909ff543b0d450
sinkin' feeling <109888201+sinkinfeeling@users.noreply.github.com>
2025-10-27 20:25:24 +0530

    Implementing selection sort implementation

--- selectionSort.c
+++ selectionSort.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
+void selectionSort(int a[], int length);
+
 int main()
 {
-  int a[] = {5, 9, 7, 6, 4, 0, 2, 3, 8, 1};
-  int length = 10;
-  
-  for (int i = 0; i < length - 1; i++)
-  {
-    int min_pos = i;
-    for (int j = i + 1; j < length; j++)
-      if (a[j] > a[min_pos]) min_pos = j;
-    
-    if (min_pos != i)
+    int a[] = {5, 9, 7, 6, 4, 0, 2, 3, 8, 1};
+    int length = 10;
+
+    selectionSort(a, length);
+
+    for (int i = 0; i < length; i++)
+        printf("a[%d] = %d\n", i, a[i]);
+
+    return 0;
+}
+
+void selectionSort(int a[], int length)
+{
+    for (int i = 0; i < length - 1; i++)
     {
-      int temp = a[i];
-      a[i] = a[min_pos];
-      a[min_pos] = temp;
+        int min_pos = i;
+        for (int j = i + 1; j < length; j++)
+            if (a[j] < a[min_pos]) min_pos = j;
+
+        if (min_pos != i)
+        {
+            int temp = a[i];
+            a[i] = a[min_pos];
+            a[min_pos] = temp;
+        }
     }
-  }
-  
-  for (int i = 0; i < length; i++)
-    printf("a[%d] = %d\n", i, a[i]);
-  
-  return 0;
 }
